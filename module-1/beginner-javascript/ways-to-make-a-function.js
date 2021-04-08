f56b237fc23118bf80ae3c3efb0ffa355aa502bd
Fran2304 <lilatiravanttiarteaga@gmail.com>
2021-04-08 13:49:27 -0500

    basic, function and dom

--- module-1/beginner-javascript/ways-to-make-a-function.js
+++ module-1/beginner-javascript/ways-to-make-a-function.js
@@ -1,24 +1,68 @@
 console.log('it does work');
 
-// Convert declarated function into arrow function
+// // Convert declarated function into arrow function
 
-// function add(a, b = 1) {
-//   const c = a + b
-//   return c
-// }
-
-
-const add = (a, b=1) => a+b
+// // function add(a, b = 1) {
+// //   const c = a + b
+// //   return c
+// // }
 
 
-// Convert declarated function with object into arrow function
+// const add = (a, b=1) => a+b
 
-// function makeABaby(first, last){
-//   const baby = {
-//     name: `${first} ${last}`,
-//     age: 0
-//   }
-//   return baby
-// }
 
-const makeABaby = (first, last) => ( {name: `${first} ${last}`, age: 0 })
+// // Convert declarated function with object into arrow function
+
+// // function makeABaby(first, last){
+// //   const baby = {
+// //     name: `${first} ${last}`,
+// //     age: 0
+// //   }
+// //   return baby
+// // }
+
+const makeABaby = (first, last) => ( {name: `${first} ${last}`, age: 0 });
+
+// IIFE
+(function(age) {
+  console.log('loading');
+  return `You are cool and age ${age}`;
+})(10);
+
+// Methods
+
+const west = {
+  name: 'Wes Bos',
+  //method
+  sayHi: function(time){
+    console.log(`Hey ${this.name} ${time}`);
+  },
+  //shorhand method
+  yellHi(){
+    console.log(`Yujuu wess`)
+  },
+
+  //arrow function . EL this, aqui se va ir al objeto windows
+  whisper: (time) => {
+    console.log(`mmmm ${this.name} ${time}`)
+  }
+}
+
+
+west.sayHi('morning');
+west.yellHi('morning');
+west.whisper('morning');
+
+// Callback function
+
+const button =  document.querySelector('.clickMe');
+
+function handle(){
+  console.log('you click me :)')
+}
+button.addEventListener('click', handle);
+
+//timer callback
+
+setTimeout(west.yellHi,1000);
+
